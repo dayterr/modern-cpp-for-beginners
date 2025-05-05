5a06fee1d9e095f058923e4c8dcb36ee366cd0df
dayterr <mvdayter@gmail.com>
2025-05-05 22:00:48 +0300

    added ch6 exercises

---
+++ ch6/initialization.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    char mychar{ 'a' };
+    int myint{ 123 };
+    double mydouble{ 456.78 };
+    std::cout << "The value of a char variable is: " << mychar << '\n';
+    std::cout << "The value of a int variable is: " << myint << '\n';
+    std::cout << "The value of a double variable is: " << mydouble << '\n'; 
+}
